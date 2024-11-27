860756e4b481f0a238eb0db3b491bb80b616d255
v8ive <v8ive369@gmail.com>
2024-11-27 02:43:42 -0500

    Update firebase.config.ts

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
