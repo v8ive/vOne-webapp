16876c66d205e60e894e2bdc598ca6b1968c16a3
v8ive <62786822+v8ive@users.noreply.github.com>
2024-12-08 21:00:22 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,36 +7,6 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-      VitePWA({
-          injectRegister: "inline",
-          registerType: 'autoUpdate',
-          manifest: {
-            name: "vOne",
-            short_name: "vOne",
-            description: "v8iveOne",
-            start_url: "/",
-            display: "standalone",
-            background_color: "#000",
-            theme_color: "#000",
-            icons: [
-                {
-                    "src": "/assets/icons/icon-96x96.png",
-                    "sizes": "96x96",
-                    "type": "image/png"
-                },
-                {
-                    "src": "/assets/icons/icon-192x192.png",
-                    "sizes": "192x192",
-                    "type": "image/png"
-                },
-                {
-                    "src": "/assets/icons/icon-512x512.png",
-                    "sizes": "512x512",
-                    "type": "image/png"
-                }
-            ]
-          },
-        }),
     ],
     css: {
         postcss: {
