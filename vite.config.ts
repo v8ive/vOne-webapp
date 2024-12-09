99a428185d1cfce048834d94772469fa38137ee6
v8ive <62786822+v8ive@users.noreply.github.com>
2024-12-08 21:02:31 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
