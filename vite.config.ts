d66fda54cd862246f2b3b4cc0136e7225834bb7b
v8ive <62786822+v8ive@users.noreply.github.com>
2024-12-08 21:53:07 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,19 +20,19 @@ export default defineConfig({
             theme_color: "#000",
             icons: [
                 {
-                    "src": "/assets/icons/icon-96x96.png",
-                    "sizes": "96x96",
-                    "type": "image/png"
+                    src: "/assets/icons/icon-96x96.png",
+                    sizes: "96x96",
+                    type: "image/png"
                 },
                 {
-                    "src": "/assets/icons/icon-192x192.png",
-                    "sizes": "192x192",
-                    "type": "image/png"
+                    src: "/assets/icons/icon-192x192.png",
+                    sizes: "192x192",
+                    type: "image/png"
                 },
                 {
-                    "src": "/assets/icons/icon-512x512.png",
-                    "sizes": "512x512",
-                    "type": "image/png"
+                    src: "/assets/icons/icon-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png"
                 }
             ]
           },
