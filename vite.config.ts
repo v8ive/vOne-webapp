da37dd6ad001f10a5ac71198548e934256d75ec4
v8ive <v8ive369@gmail.com>
2024-12-08 23:26:31 -0500

    Adjustments

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,54 @@ export default defineConfig({
                     src: "/assets/icons/icon-512x512-transparent.png",
                     sizes: "512x512",
                     type: "image/png"
+                },
+                {
+                    purpose: "maskable",
+                    sizes: "1046x1046",
+                    src: "/assets/icons/maskable_icon.png",
+                    type: "image/png"
+                },
+                {
+                    purpose: "maskable",
+                    sizes: "48x48",
+                    src: "/assets/icons/maskable_icon_x48.png",
+                    type: "image/png"
+                },
+                {
+                    purpose: "maskable",
+                    sizes: "72x72",
+                    src: "/assets/icons/maskable_icon_x72.png",
+                    type: "image/png"
+                },
+                {
+                    purpose: "maskable",
+                    sizes: "96x96",
+                    src: "/assets/icons/maskable_icon_x96.png",
+                    type: "image/png"
+                },
+                {
+                    purpose: "maskable",
+                    sizes: "128x128",
+                    src: "/assets/icons/maskable_icon_x128.png",
+                    type: "image/png"
+                },
+                {
+                    purpose: "maskable",
+                    sizes: "192x192",
+                    src: "/assets/icons/maskable_icon_x192.png",
+                    type: "image/png"
+                },
+                {
+                    purpose: "maskable",
+                    sizes: "384x384",
+                    src: "/assets/icons/maskable_icon_x384.png",
+                    type: "image/png"
+                },
+                {
+                    purpose: "maskable",
+                    sizes: "512x512",
+                    src: "/assets/icons/maskable_icon_x512.png",
+                    type: "image/png"
                 }
             ]
           },
