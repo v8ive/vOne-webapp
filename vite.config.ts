e002fb2541378446dd3461e0e5aee11aa13ab47b
v8ive <62786822+v8ive@users.noreply.github.com>
2024-12-08 19:00:22 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
             description: "v8iveOne",
             start_url: "/",
             display: "standalone",
-            backgroundColor: "#000000",
-            themeColor: "#000000",
+            background_color: "#000000",
+            theme_color: "#000000",
             icons: [
                 {
                     "src": "/assets/icons/icon-96x96.png",
