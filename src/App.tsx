3e044b405c0917bc7f8b1728e1159c2cb060bde0
v8ive <v8ive369@gmail.com>
2024-12-09 20:30:04 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import { Theme } from "@radix-ui/themes";
 
 import './App.css';
 import { useCustomTheme } from "./context/CustomThemeContext";
-import { Vortex } from "./components/ui/vortex";
 
 function App() {
     const { initialize } = useAuthStore();
@@ -41,35 +40,6 @@ function App() {
         initialize();
     }, []);
 
-    const colorHues = {
-        'gray': 0,
-        'gold': 40,
-        'bronze': 30,
-        'brown': 20,
-        'yellow': 50,
-        'amber': 60,
-        'orange': 30,
-        'tomato': 10,
-        'red': 5,
-        'ruby': 6,
-        'crimson': 320,
-        'pink': 310,
-        'plum': 300,
-        'purple': 280,
-        'violet': 270,
-        'iris': 260,
-        'indigo': 250,
-        'blue': 240,
-        'cyan': 200,
-        'teal': 180,
-        'jade': 160,
-        'green': 140,
-        'grass': 120,
-        'lime': 100,
-        'mint': 190,
-        'sky': 230
-    }
-
     return (
         <Theme
             hasBackground
@@ -77,13 +47,6 @@ function App() {
             accentColor={themeName}
             panelBackground='translucent'
         >
-            <Vortex
-                backgroundColor={mode === 'dark' ? 'rgba(0, 0, 0, 0.5)' : 'transparent'}
-                rangeY={800}
-                particleCount={10}
-                baseHue={colorHues[themeName]}
-                baseSpeed={0.1}
-            />
             <Header />
             <Routes>
                 <Route path="/" element={<HomePage />} />
